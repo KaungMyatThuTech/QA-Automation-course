c127636f3a71ec1ce0fe4e6e5021782a74f04c01
kaung myat <kaungmthuqa@gmail.com>
2026-01-24 22:20:12 -0800

    String.js

--- week-2/String.js
+++ week-2/String.js
@@ -1,23 +1,87 @@
-// 1. ToUpperCase() - converts a string to uppercase letters
+// Creating strings with single quotes, double quotes, and backticks
 
-const kaungName = "kaung myat";
-console.log(kaungName.toUpperCase()); // KAUNG MYAT
+let str1 = "Hey";
+let str2 = "there";
+let str3 = `Good Morning`;
+console.log(str1 + " " + str2 + " " + str3 + "!");
+console.log(`${str1} ${str2} ${str3} !`);
 
-// 2. ToLowerCase() - converts a string to lowercase letters
+// 1 . string length
+let personName = "Kaung Myat";
+console.log(personName.length);
 
-const RICHARD_NAME = "RICHARD";
-console.log(RICHARD_NAME.toLowerCase()); // richard
+// 2. Accessing characters
+let askingString = "How are you?";
+console.log(askingString[0]);
+console.log(askingString[7]);
 
-// 3. Trim() - removes whitespace from both ends of a string
+// 3 . Changing case
 
-let messyString = " JavaScript ";
-console.log(messyString); // " JavaScript "
-console.log(messyString.trim()); // "JavaScript"
+let greeting = "Howdee!";
+console.log(greeting.toUpperCase());
+console.log(greeting.toLowerCase());
 
-//4. includes() - checks if a string contains a specified value
+// 4. Trim() - removes whitespace from both ends of a string
 
-const sentence = "The quick brown fox jumps over the lazy dog.";
-console.log(sentence.includes("fox")); // true
-console.log(sentence.includes("cat")); // false
+let messy = " JavaScript ";
+console.log(messy);
+console.log(messy.trim());
 
-// 5. slice() - extracts a section of a string and returns it as a new string
+// 5. Slice(start, end)
+let text = "TojTech!";
+console.log(text.slice(0, 3));
+console.log(text.slice(3, 7));
+
+// 6. Replace() - replaces a specified value with another value in a string
+let welcomeMessage = "Welcome to TojTech!";
+console.log(welcomeMessage.replace("TojTech", "JavaScript"));
+
+// 7. includes(substriing)
+const sentence = "The brown fox jumps over the lazy dog.";
+console.log(sentence.includes("fox"));
+console.log(sentence.includes("cat"));
+
+// 8. split(separator)
+let vegetables = "Carrot, Potato, Tomato, Cabbage";
+console.log(vegetables.split(" / "));
+
+// 9. concat(string1,string2,....)
+let firstName = "Kaung";
+let lastName = "Myat";
+console.log(firstName.concat(" ", lastName));
+
+// 10 . indexOf(substring)
+let sentence1 = "Learning JavaScript is fun!";
+console.log(sentence1.indexOf("JavaScript"));
+console.log(sentence1.indexOf("Python"));
+
+// 12. startsQith() and endsWith()
+let filename = "document.pdf";
+console.log(filename.startsWith("doc"));
+console.log(filename.endsWith(".pdf"));
+console.log(filename.endsWith(".doc"));
+
+// Template Literals
+let userName = "Kaung Myat";
+let greetingMessage = `Hello, ${userName}! Welcome to the JavaScript world.`;
+console.log(greetingMessage);
+
+// Multi-line strings .
+let poem = `stars are bright
+  the night is calm
+  the world is beautiful`;
+console.log(poem);
+
+// 2. Expressions in template literals
+let N = 5;
+let M = 10;
+console.log(`The sum of ${N} and ${M} is ${N + M}.`);
+
+// 3. Combining String Methods + Template Literals
+let userName2 = "  kaung myat  ";
+let jvSkillLevel = "80";
+console.log(
+  `Hello, ${userName2.trim().toUpperCase()}!
+   Your JavaScript skill level is 
+   ${parseInt(jvSkillLevel) + 10}%.`,
+);
